8f16c3f1d5a22c3e09361559c6c9cfd54addb31b
archeopternix <39010055+archeopternix@users.noreply.github.com>
2019-12-22 13:45:58 +0100

    Textarea added

--- entity.go
+++ entity.go
@@ -20,11 +20,12 @@ type Field struct {
 	Type      string `json:"type"` // string, int, bool, lookup, tel, email
 	Object    string `json:"object,omitempty"`
 	Maxlength int    `json:"maxlength,omitempty"`
-	Size      int    `json:"size,omitempty"`
+	Size      int    `json:"size,omitempty"` // for textarea size = cols
 	Required  bool   `json:"required"`
 	Step      int    `json:"step,omitempty"` //for Number fields
 	Min       int    `json:"min,omitempty"`  //for Number fields
 	Max       int    `json:"max,omitempty"`  //for Number fields
+	Rows      int    `json:"rows,omitempty"` //for textarea
 }
 
 func NewEntity() (e *Entity) {
